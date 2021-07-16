2763d066c493f30d577077588bc55c3815aa22b9
Tony Fang <galaxies@vip.qq.com>
2021-07-16 20:00:46 +0800

    upd

--- depth_renderer.py
+++ depth_renderer.py
@@ -79,7 +79,7 @@ class SceneRenderer(object):
 
 if __name__ == '__main__':
     renderer = SceneRenderer()
-    depth = renderer.render_image('data/0')
+    depth = renderer.render_image('data/scene1/0/')
     depth = depth / depth.max()
     cv2.imshow('depth', depth)
     cv2.waitKey(0)
