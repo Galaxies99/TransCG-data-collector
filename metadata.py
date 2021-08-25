cd2f1f468a48e795e9d4ce176e5331d07dff5451
Tony Fang <galaxies@vip.qq.com>
2021-08-25 14:04:47 +0800

    upd

--- metadata.py
+++ metadata.py
@@ -19,3 +19,70 @@ for id in range(1, 131):
         meta["type"] = "cluttered"
     with open(os.path.join('data', 'scene{}'.format(id), 'metadata.json'), 'w') as fp:
         json.dump(meta, fp)
+
+train_set = []
+cluttered = []
+for id in range(1, 131):
+    if id not in test_set:
+        train_set.append(id)
+    if id not in isolated:
+        cluttered.append(id)
+
+train_D435_cnt = 0
+train_L515_cnt = 0
+test_D435_cnt = 0
+test_L515_cnt = 0
+isolated_D435_cnt = 0
+isolated_L515_cnt = 0
+cluttered_D435_cnt = 0
+cluttered_L515_cnt = 0
+
+for scene_id in range(1, 131):
+    if scene_id in train_set:
+        with open(os.path.join('data', 'scene{}'.format(scene_id), "metadata.json"), 'r') as fp:
+            meta = json.load(fp)
+            train_D435_cnt += meta['D435_valid_perspective_num']
+            train_L515_cnt += meta['L515_valid_perspective_num']
+    else:
+        with open(os.path.join('data', 'scene{}'.format(scene_id), "metadata.json"), 'r') as fp:
+            meta = json.load(fp)
+            test_D435_cnt += meta['D435_valid_perspective_num']
+            test_L515_cnt += meta['L515_valid_perspective_num']
+    if scene_id in isolated:
+        with open(os.path.join('data', 'scene{}'.format(scene_id), "metadata.json"), 'r') as fp:
+            meta = json.load(fp)
+            isolated_D435_cnt += meta['D435_valid_perspective_num']
+            isolated_L515_cnt += meta['L515_valid_perspective_num']
+    else:
+        with open(os.path.join('data', 'scene{}'.format(scene_id), "metadata.json"), 'r') as fp:
+            meta = json.load(fp)
+            cluttered_D435_cnt += meta['D435_valid_perspective_num']
+            cluttered_L515_cnt += meta['L515_valid_perspective_num']
+
+assert train_D435_cnt + test_D435_cnt == isolated_D435_cnt + cluttered_D435_cnt
+assert train_L515_cnt + test_L515_cnt == isolated_L515_cnt + cluttered_L515_cnt
+
+meta = {
+    "total_samples": train_D435_cnt + train_L515_cnt + test_D435_cnt + test_L515_cnt,
+    "total_D435_samples": train_D435_cnt + test_D435_cnt,
+    "total_L515_samples": train_L515_cnt + test_L515_cnt,
+    "train": train_set,
+    "train_samples": train_D435_cnt + train_L515_cnt,
+    "train_D435_samples": train_D435_cnt,
+    "train_L515_samples": train_L515_cnt,
+    "test": test_set,
+    "test_samples": test_D435_cnt + test_L515_cnt,
+    "test_D435_samples": test_D435_cnt,
+    "test_L515_samples": test_L515_cnt,
+    "isolated": isolated,
+    "isolated_samples": isolated_D435_cnt + isolated_L515_cnt,
+    "isolated_D435_samples": isolated_D435_cnt,
+    "isolated_L515_samples": isolated_L515_cnt,
+    "cluttered": cluttered,
+    "cluttered_samples": cluttered_D435_cnt + cluttered_L515_cnt,
+    "cluttered_D435_samples": cluttered_D435_cnt,
+    "cluttered_L515_samples": cluttered_L515_cnt 
+}
+
+with open(os.path.join('data', 'metadata.json'), 'w') as fp:
+    json.dump(meta, fp)
